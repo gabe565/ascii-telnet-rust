69bff075d8a600dc147ac2b876f05eb9adedf8db
Gabe Cook <gabe565@gmail.com>
2021-08-07 02:23:53 -0500

    :sparkles: Keep frame in current term location instead of clearing entire term

--- src/movie_client.rs
+++ src/movie_client.rs
@@ -5,14 +5,14 @@ use async_std::task::sleep;
 use futures::AsyncWriteExt;
 use log::info;
 use nanoid::nanoid;
-use termion::{cursor, color};
+use termion::color;
 
 const FRAME_HEIGHT: u16 = 13;
 const HEIGHT: u16 = 14;
 const WIDTH: u16 = 67;
 
-const PAD_LEFT: u16 = 6;
-const PAD_TOP: u16 = 4;
+const PAD_X: u16 = 6;
+const PAD_Y: u16 = 3;
 
 const MOVIE: &str = include_str!("../movies/sw1.txt");
 
@@ -38,18 +38,10 @@ impl MovieClient {
     }
 
     async fn clear(&mut self) -> async_std::io::Result<usize> {
-        self.stream.write(format!("{}", termion::clear::All).as_bytes()).await
+        self.stream.write(format!("{}", termion::clear::AfterCursor).as_bytes()).await
     }
 
-    fn build_line(x: u16, y: u16, text: String) -> String {
-        format!(
-            "{}{}\n",
-            cursor::Goto(x, y),
-            text,
-        )
-    }
-
-    fn progress_bar(y: u16, line_num: usize, total: usize) -> String {
+    fn progress_bar(line_num: usize, total: usize) -> String {
         let percent = (line_num as f32) / (total as f32);
         let whole_width = percent * (WIDTH as f32);
         let part_char = match (whole_width % 1.0 * 8.0) as u16 {
@@ -63,18 +55,23 @@ impl MovieClient {
             7 => "▉",
             _ => "█",
         };
-        Self::build_line(PAD_LEFT - 1, y, format!(
-            "{}[{}{}{}]{}",
+        let pad = "\n".repeat(PAD_Y as usize);
+        format!(
+            "{}{}{}[{}{}{}]{}{}",
+            pad,
+            " ".repeat((PAD_X - 1) as usize),
             color::Fg(color::LightBlack),
             "█".repeat(whole_width as usize),
             part_char,
             " ".repeat((WIDTH as usize) - (whole_width as usize) - 1),
-            color::Fg(color::Reset)
-        ))
+            color::Fg(color::Reset),
+            pad,
+        )
     }
 
     async fn stream(&mut self) -> async_std::io::Result<()> {
         self.clear().await?;
+        self.stream.write("\n".repeat(PAD_Y as usize).as_bytes()).await?;
         let line_count = MOVIE.split("\n").count();
         let mut sleep_time: u64 = 0;
         let mut buffer = Vec::with_capacity(HEIGHT as usize);
@@ -82,17 +79,23 @@ impl MovieClient {
             match (i as u16) % HEIGHT {
                 0 => sleep_time = line.parse::<u64>().unwrap() * 1000 / 15,
                 curr_line => {
-                    buffer.push(Self::build_line(
-                        PAD_LEFT, curr_line + PAD_TOP, line.to_string()
+                    buffer.push(format!(
+                        "{}{}\n",
+                        " ".repeat(PAD_X as usize),
+                        line.to_string()
                     ));
                     if curr_line == FRAME_HEIGHT {
                         buffer.push(Self::progress_bar(
-                            curr_line + PAD_TOP + 3, i, line_count
+                            i, line_count
                         ));
                         self.stream.write(buffer.concat().as_bytes()).await?;
                         buffer.clear();
                         sleep(Duration::from_millis(sleep_time)).await;
-                        buffer.push(format!("{}", termion::clear::All));
+                        buffer.push(format!(
+                            "{}{}",
+                            termion::cursor::Up(FRAME_HEIGHT + PAD_Y * 2),
+                            termion::clear::AfterCursor,
+                        ));
                     }
                 }
             }
